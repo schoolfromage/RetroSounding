b1f1c4c27ecb45ea45bb551d89b864fea245e907
Dingus <prodiginus@yahoo.com>
2020-12-02 15:11:23 -0800

    Changed a lot, mainly added entry pages and a "retrieve" function for the whoosh class to get specific games from the search

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -23,14 +23,21 @@ def results():
 	keywordquery = data.get('searchterm')
 	print('Keyword Query is: ' + keywordquery)
 
-	name, release_year = mysearch.search(keywordquery)
-	return render_template('results.html', query=keywordquery, results=zip(name, release_year))
+	name, release_year, publishers, gids = mysearch.search(keywordquery)
+	return render_template('results.html', query=keywordquery, results=zip(name, release_year, publishers, gids))
 
 @app.route('/entry/', methods=['GET', 'POST'])
 def entry():
-	#TODO whoosh stuff
+	global mysearch
+	if request.method == 'POST':
+		data = request.form
+	else:
+		data = request.args
+	
+	gid = data.get('gid')
 
-	return render_template('entry.html')
+	name, release_year, developers, publishers, images, sources, genres, description = mysearch.retrieve(gid)
+	return render_template('entry.html', name=name, release_year=release_year, developers=developers, publishers=publishers, images=images, sources=sources, genres=genres, description=description)
 
 class MyWhooshSearch(object):
 	"""docstring for MyWhooshSearch"""
@@ -46,6 +53,7 @@ class MyWhooshSearch(object):
 		sources = list()
 		genres = list()
 		description = list()
+		gids = list()
 		# consoles = list()
 		searchFields = ['name', 'release_year', 'developers', 'publishers', 'genres', 'description']
 
@@ -63,9 +71,27 @@ class MyWhooshSearch(object):
 				sources.append(x['sources'])
 				genres.append(x['genres'])
 				description.append(x['description'])
+				gids.append(x['GID'])
 
-		return name, release_year
-	
+		return name, release_year, publishers, gids
+	def retrieve(self, gid):
+		with self.indexer.searcher() as search:
+			parser = MultifieldParser(['GID'], schema=self.indexer.schema)
+			query = parser.parse(str(gid))
+			result = search.search(query, limit=None)
+			print(result)
+
+			name = result[0]['name']
+			release_year = result[0]['release_year']
+			developers = result[0]['developers']
+			publishers = result[0]['publishers']
+			images = result[0]['images']
+			sources = result[0]['sources']
+			genres = result[0]['genres']
+			description = result[0]['description']
+
+		return name, release_year, developers, publishers, images, sources, genres, description
+
 	def index(self):
 		# schema = Schema(GID=ID(stored=True, unique=True),name=KEYWORD(stored=True),release_year=NUMERIC(stored=True),developers=KEYWORD(stored=True, commas=True),publishers=KEYWORD(stored=True, commas=True),images=KEYWORD(stored=True, commas=True),sources=KEYWORD(stored=True, commas=True),genres=KEYWORD(stored=True, commas=True),description=TEXT(stored=True))
 		if whoosh.index.exists_in("./backend/GamesIndex"):
