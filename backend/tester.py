278663c7759d606a54ca6e7dbf0acde0f04c9a47
technofromage <53595974+technofromage@users.noreply.github.com>
2020-12-02 17:10:20 -0800

    fixed problems with genres and re-imported data

---
+++ backend/tester.py
@@ -0,0 +1,35 @@
+from whoosh import index
+from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED, NUMERIC	#for creating the schema
+from whoosh.query import Query, And, Not, Term, Phrase, Every	#for searching the database for douplicates
+from whoosh.qparser import QueryParser
+from whoosh.analysis import SimpleAnalyzer
+
+
+schema = Schema(GID=ID(stored=True, unique=True),#GID = Game ID
+	name=TEXT(stored=True, analyzer=SimpleAnalyzer()),#standard analizer cuts out words that simpleanalyzer keeps; words like "the" and "3-d" or "8"
+	release_year=NUMERIC(stored=True),
+	developers=KEYWORD(stored=True, commas=True, lowercase=True),
+	publishers=KEYWORD(stored=True, commas=True, lowercase=True),
+	images=KEYWORD(stored=True, commas=True),
+	consoles=KEYWORD(stored=True, commas=True, lowercase=True),
+	sources=KEYWORD(stored=True, commas=True),
+	genres=KEYWORD(stored=True, commas=True, lowercase=True),
+	description=TEXT(stored=True)
+	)
+	
+if not index.exists_in('GamesIndex'):
+	idx = index.create_in('GamesIndex',schema = schema)
+else:
+	idx = index.open_dir('GamesIndex')	
+searcher = idx.searcher()
+QP = QueryParser("name",schema=schema)
+
+q = And([Term('genres', ''),Not(Term('consoles','nes'))])
+results = searcher.search(q)
+print(results)
+for result in results:
+	print(result['GID'],result['name'],result['consoles'],result['genres'])
+
+numdocs = searcher.doc_count_all()
+print(numdocs)
+searcher.close()
--- backend/wiki_cleaner.py
+++ backend/wiki_cleaner.py
@@ -3,8 +3,8 @@ from sys import argv
 
 #Steven A
 #THE "CORRECT" REGEX:
-#([^,]*),"(.*)",([^,]*),\[(.*)\],\[(.*)\],"(.*)",\[(.*)\],\[(.*)\],(".*")
-#id ,"name",release_year,[developers],[publishers],"image",[src],[genres],"description"
+#([^,]*),"(.*)",([^,]*),\[(.*)\],\[(.*)\],"(.*)",\[(.*)\],\[(.*)\],\[(.*)\],"(.*)"
+#id ,"name",release_year,[developers],[publishers],"image",[src],[genres],[consoles],"description"
 
 #please do not use this to overwrite the original csv before checking to make sure your changes are what you wanted
 
@@ -19,9 +19,8 @@ outputLines.append(List[0])
 for line in List[1:]:
 	match = re.match(r'([^,]*),"(.*)",([^,]*),\[(.*)\],\[(.*)\],"(.*)",\[(.*)\],\[(.*)\],\[(.*)\],"(.*)"',line)
 	if match:
-		src = match.group(7).replace('wikipedia.org/','wikipedia.org/wiki/').replace(' ','_')
-		pic = match.group(6).replace('//w','/w')
-		outputLines.append(match.group(1)+','+'"'+match.group(2)+'"'+','+match.group(3)+','+'['+match.group(4)+']'+','+'['+match.group(5)+']'+','+'"'+pic+'"'+','+'['+src+']'+','+'['+match.group(8)+']'+','+'['+match.group(9)+']'+','+'"'+match.group(10)+'"'+'\n')
+		genres = match.group(8).replace(', ,',',').replace(',,',',').replace(', ',',').replace(' ,',',')
+		outputLines.append(match.group(1)+','+'"'+match.group(2)+'"'+','+match.group(3)+','+'['+match.group(4)+']'+','+'['+match.group(5)+']'+','+'"'+match.group(6)+'"'+','+'['+match.group(7)+']'+','+'['+genres+']'+','+'['+match.group(9)+']'+','+'"'+match.group(10)+'"'+'\n')
 	else:
 		print("Error in match\n",line)
 		break;
