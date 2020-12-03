f3efe1ece537f01529910b1fa35c0e2777f3a0a0
technofromage <53595974+technofromage@users.noreply.github.com>
2020-12-03 01:45:55 -0800

    pictures uploaded, and scrapers updated to get pictures

--- backend/wiki_to_csv_GB.py
+++ backend/wiki_to_csv_GB.py
@@ -14,7 +14,7 @@ import re
 
 #Steven A
 starter = "https://en.wikipedia.org/w/api.php?action=parse&page="	#used for all queries
-URLstarter = "https://en.wikipedia.org/"	#used for the investigate_further and the URL
+URLstarter = "https://en.wikipedia.org/wiki/"	#used for the investigate_further and the URL
 
 def manageArgs():
 	if (len(argv)!=4):
@@ -32,10 +32,10 @@ def manageArgs():
 		
 def investigate_further(Target):
 	if (Target == None):
-		return "n/a","n/a","n/a","n/a"#return junk
+		return "n/a","n/a","[n/a]","n/a"#return junk
 	#split the section name (#) from the target
 	Split = Target.split('#')
-	Name = Split[0]
+	Name = Split[0].replace('/wiki/',''	)
 	if (len(Split)>1):
 		SectionNumb = get_section_number(Name, Split[1])
 	else:
@@ -49,13 +49,13 @@ def investigate_further(Target):
 	Listfile = json.loads(req.text)
 	if "error" in Listfile:
 		(Name+" is not on wikipedia")
-		return "n/a","n/a","n/a","n/a"
+		return "n/a","n/a","[n/a]","n/a"
 	Text = Listfile["parse"]["text"]["*"]
 	store = lxml.html.fromstring(Text)
-	URL = URLstarter+Listfile["parse"]["title"]
-	TempList = store.xpath("//a[@class='image']/@href")
+	URL = URLstarter+Listfile["parse"]["title"].replace('_',',')
+	TempList = store.xpath("//table[contains(@class,'infobox')]//a[@class='image']/img/@src")
 	if len(TempList)>=1:
-		Picture =  URLstarter+TempList[0]
+		Picture = TempList[0]
 	else:
 		Picture = "n/a"
 	Genres = store.xpath("//th/a[text()='Genre(s)']/../following::td[1]//text()")
@@ -69,7 +69,8 @@ def investigate_further(Target):
 		Discription = Paragraphs[0].text_content().replace("\n", "")#no newlines!
 	else:#default behavior
 		if (len(Paragraphs)>=2):
-			Discription = Paragraphs[0].text_content().replace("\n", "")+" \\n "+Paragraphs[1].text_content().replace("\n", "")
+			Discription = Paragraphs[0].text_content().replace("\n", "")+" <br /> "+Paragraphs[1].text_content().replace("\n", "")
+			Discription = re.sub(r'\[[^\[]*\]','',Discription)
 		else:
 			Discription = "n/a"
 	return URL, Picture, Genres, Discription
